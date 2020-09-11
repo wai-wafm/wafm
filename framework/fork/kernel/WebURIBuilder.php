6b1ab481b17645aa88c3712066119464f017fff1
wai-wafm <wafm.framework@yandex.ru>
2020-09-11 18:42:18 +0500

    update #0006

---
+++ framework/fork/kernel/WebURIBuilder.php
@@ -0,0 +1,16 @@
+<?php
+namespace wafm\framework\fork\kernel;
+
+class WebURIBuilder 
+{    
+    public function __construst()
+    {
+        
+    }
+    
+    private function primary()
+    {
+ 
+    }
+    
+}
